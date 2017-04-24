757d79bdd979097c75c0f752ff32d96380199bef
keerthinagareddy10 <ln555r@att.com>
2017-04-24 21:09:15 +0530

    Initial version

---
+++ Devopsgit3/src/samplejavaPrg/AddTwoNum.java
@@ -0,0 +1,18 @@
+package samplejavaPrg;
+
+public class AddTwoNum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,sum;
+		a=39;
+		b=45;
+		sum=a+b;
+		System.out.println("Sum of two numbers:"+sum);
+				
+	}
+
+}
